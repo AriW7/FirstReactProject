5145de5252f8feda07f23a6ae4261b6775ccdc95
Arielle Williams <ariellewilliams777@gmail.com>
2023-02-07 18:38:14 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my very first React project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AriW7"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Here's my GitHub
         </a>
       </header>
     </div>
